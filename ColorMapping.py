ea3ea551203171b814f0a44487b65b1d0f2f82ba
AitanaBC <aitanabeldacamos@gmail.com>
2022-03-12 19:54:19 +0100

    set_dictionary

--- ColorMapping.py
+++ ColorMapping.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#Aitana Belda VF013 Scripts II
+
 import maya.cmds as cmds
 
 
-
-
+#DICTIONARY
+class ColorMapping(dict):
+    def __init__(self):
+        self[data] = {
+            'gray':[0,(0.4,0.4,0.4)],
+            'black':[1,(0,0,0)],
+            'darkGray':[2,(.35,.35,.35)],
+            'lightGray':[3,(.43,.43,.43)],
+            'burgundy':[4,(.54,0,.11)],
+            'darkBlue':[5,(0,0,.31)],
+            'blue':[6,(0,0,1)],
+            'darkGreen':[7,(0,.22,0.07)],
+            'purple':[8,(.11,0,.2)],
+            'pink':[9,(.74,0,.75)],
+            'midBrown':[10,(.47,.22,.15)],
+            'darkBrown':[11,(.19,.1,.09)],
+            'rust':[12,(.53,.09,0)],
+            'red':[13,(1,0,0)],
+            'green':[14,(0,1,0)],
+            'cobalt':[15,(0,.16,.54)], 
+            'white':[16,(1,1,1)],
+            'yellow':[17,(1,1,0)],
+            'turquoise':[18,(.31,.83,1)],
+            'springGreen':[19,(.18,1,.56)],
+            'lightPink':[20,(1,.62,.62)],
+            'sandyBrown':[21,(.87,.62,.39)],
+            'paleYellow':[22,(1,1,.27)],
+            'jade':[23,(0,.55,.25)],
+            'lightbrown':[24,(.56,.34,.13)],
+            'olive':[25,(.56,.58,.11)],
+            'appleGreen':[26,(.33,.58,.12)],
+            'seaGreen':[27,(.13,.58,.29)],
+            'teal':[28,(.13,.57,.56)],
+            'cerulean':[29,(.14,.32,.57)],
+            'darkViolet':[30,(.36,.07,.57)],
+            'eggPlant':[31,(.57,.11,.34)]
+            }
